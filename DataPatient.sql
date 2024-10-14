00a61fc5d1b2f73f0f326ff76190dff001883588
airwin17 <faitourdja@gmail.com>
2024-10-14 18:13:34 +0200

    fixing sql file

--- DataPatient.sql
+++ DataPatient.sql
@@ -1,6 +1,8 @@
+CREATE DATABASE  IF NOT EXISTS `patienttest` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `patienttest`;
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
--- Host: localhost    Database: paymybuddydb
+-- Host: localhost    Database: patienttest
 -- ------------------------------------------------------
 -- Server version	8.0.38
 
@@ -16,79 +18,22 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `bank_account`
+-- Table structure for table `patient`
 --
 
-DROP TABLE IF EXISTS `bank_account`;
+DROP TABLE IF EXISTS `patient`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bank_account` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `balance` double NOT NULL,
-  `iban` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `iban_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=135 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `connections`
---
-
-DROP TABLE IF EXISTS `connections`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `connections` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `userID1` int NOT NULL,
-  `userID2` int NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user1fk_idx` (`userID1`),
-  KEY `user2fk_idx` (`userID2`),
-  CONSTRAINT `user1fk` FOREIGN KEY (`userID1`) REFERENCES `user` (`id`),
-  CONSTRAINT `user2fk` FOREIGN KEY (`userID2`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=108 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `transaction`
---
-
-DROP TABLE IF EXISTS `transaction`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `transaction` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `sender` int DEFAULT NULL,
-  `receiver` int DEFAULT NULL,
-  `description` varchar(200) DEFAULT NULL,
-  `amount` double NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `senderfk_idx` (`sender`),
-  KEY `receiverfk_idx` (`receiver`),
-  CONSTRAINT `receiverfk` FOREIGN KEY (`receiver`) REFERENCES `user` (`id`),
-  CONSTRAINT `senderfk` FOREIGN KEY (`sender`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb3;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `bank_acount_id` int DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `bafk_idx` (`bank_acount_id`),
-  CONSTRAINT `bafk` FOREIGN KEY (`bank_acount_id`) REFERENCES `bank_account` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=264 DEFAULT CHARSET=utf8mb3;
+CREATE TABLE `patient` (
+  `patientid` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(45) NOT NULL,
+  `lastname` varchar(45) NOT NULL,
+  `gender` varchar(6) NOT NULL,
+  `birthdate` date NOT NULL,
+  `phone` varchar(15) DEFAULT NULL,
+  `zipcode` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`patientid`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -100,4 +45,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-22  0:06:03
+-- Dump completed on 2024-10-14 18:12:17
